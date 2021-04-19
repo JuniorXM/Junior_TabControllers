322ed275066eab77b7b3352020352b29d520c5fe
Junior <Jmorocho972@gmail.com>
2021-04-19 09:39:34 -0400

    Initial Commit

---
+++ Junior_TabControllers/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Junior_TabControllers
+//
+//  Created by Junior Morocho on 4/19/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        tabBarItem.badgeValue = "!"
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {         super.viewWillDisappear(animated);         tabBarItem.badgeValue = nil }
+}
+
+
